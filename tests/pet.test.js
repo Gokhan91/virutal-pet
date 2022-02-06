b9df427a97cb5dbd11c3443e11cc67e939dcd216
Gokhan91 <g.sancar91@gmail.com>
2022-02-06 19:48:36 +0200

    im awesome

--- tests/pet.test.js
+++ tests/pet.test.js
@@ -16,13 +16,21 @@ describe('constructor', () => {
         const pet = new Pet('Fido');
         expect(pet.age).toEqual(0);
     });
-});
-describe('growUp', () => {
-    it('increments the age by 1', () => {
-        const pet = new Pet('Fido');
+    describe('constructor', () => {
+        it('has a initial fitness of 10', () => {
+            const pet = new Pet('Fido');
+            expect(pet.fitness).toEqual(10);
+        });
+    });
+    describe('growUp', () => {
+        it('increments the age by 1', () => {
+            const pet = new Pet('Fido');
 
-        pet.growUp();
+            pet.growUp();
 
-        expect(pet.age).toEqual(1);
+            expect(pet.age).toEqual(1);
+            expect(pet.hunger).toEqual(5);
+            expect(pet.fitness).toEqual(7);
+        });
     });
 });
