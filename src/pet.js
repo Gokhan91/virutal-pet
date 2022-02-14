b1797550506d14a4a5677cd9015faf795b1e740f
Gokhan91 <g.sancar91@gmail.com>
2022-02-14 20:48:10 +0200

    I'M AWESOME as always

--- src/pet.js
+++ src/pet.js
@@ -1,16 +1,22 @@
 function Pet(name, age) {
     this.name = name;
-    this.age = age
+    this.age = 0;
     this.hunger = 0
     this.fitness = 10
 }
 Pet.prototype.growUp = function () {
+    if (!this.isAlive()) {
+        throw new Error('Your pet is no longer alive :(');
+    }
     this.age += 1;
     this.hunger += 5;
     if ((this.fitness - 3) < 0) { this.fitness = 0; }
     else this.fitness -= 3;
 };
 Pet.prototype.walk = function () {
+    if (!this.isAlive()) {
+        throw new Error('Your pet is no longer alive :(');
+    }
     if (this.fitness === 7) { this.fitness += 3 };
     if (this.fitness === 8) { this.fitness += 2 };
     if (this.fitness === 9) { this.fitness += 1 };
@@ -20,11 +26,17 @@ Pet.prototype.walk = function () {
 };
 
 Pet.prototype.feed = function () {
+    if (!this.isAlive()) {
+        throw new Error('Your pet is no longer alive :(');
+    }
     if ((this.hunger - 3) < 0) { this.hunger = 0; }
     else this.hunger -= 3;
 };
 
 Pet.prototype.checkUp = function () {
+    if (!this.isAlive()) {
+        throw new Error('Your pet is no longer alive :(');
+    }
     if (this.fitness <= 3 && this.hunger >= 5) return "I am hungry AND I need a walk";
     if (this.fitness <= 3) return "I need a walk";
     if (this.hunger >= 5) return "I am hungry";
